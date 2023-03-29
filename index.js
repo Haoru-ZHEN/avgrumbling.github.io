32f3a42159f06b220c414921a0e883e76b654e47
Zhen <106744948+Haoru-ZHEN@users.noreply.github.com>
2023-03-29 20:34:52 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
+const firebaseConfig = {
+     apiKey: "AIzaSyCvKsl6dpisURv3Yu7OMV37YyqBd2SWRok",
+     authDomain: "welogin-ef928.firebaseapp.com",
+     databaseURL: "https://welogin-ef928-default-rtdb.firebaseio.com",
+     projectId: "welogin-ef928",
+     storageBucket: "welogin-ef928.appspot.com",
+     messagingSenderId: "882804397149",
+     appId: "1:882804397149:web:6561bcc09af5130ae429b4"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+//get element
 const wrapper = document.getElementById("wrapper");
+const idInput = document.getElementById("idInput");
+const pwdInput = document.getElementById("pwdInput");
+const loginBut = document.getElementById("loginBut");
+const theform = document.getElementById("theform");
 
-/*
-const loginPopBut = document.getElementById("loginPopBut");
-
-loginPopBut.addEventListener('click',()=>{
-     wrapper.classList.add('active-popup');
-})*/
-
+//function
 function popLogin(){
      wrapper.classList.add('active-popup');
     
@@ -15,3 +27,37 @@ function popLogin(){
 function closeLogin(){
      wrapper.classList.remove('active-popup');
 }
+
+function login(){
+     authenticate(idInput.value,pwdInput.value);
+}
+
+function authenticate(idget,phraseget){
+     const dbref = firebase.database().ref();
+     const thelist = dbref.child("steins;gate");
+     //console.log(encryptPwd("始祖の巨人"));
+
+     thelist.once("value",function(snapshot){
+          thedata = snapshot.val();
+          var id = snapshot.key;
+
+          if(idget == id && phraseget == decryptPwd(thedata,idget)){
+               //window.location.href = "/katawaredoki/time.html"
+               window.open("katawaredoki/time.html");
+          }
+     })
+}
+/*
+function encryptPwd(cipher){
+     var pass12 = CryptoJS.AES.encrypt(cipher,"steins;gate"); //(text, key)
+     return pass12.toString();
+}*/
+
+function decryptPwd(passphrase, thekey){
+     var pass12 = CryptoJS.AES.decrypt(passphrase,thekey);
+     return pass12.toString(CryptoJS.enc.Utf8);
+}
+
+theform.addEventListener("submit",function(event){
+     event.preventDefault();
+ })
